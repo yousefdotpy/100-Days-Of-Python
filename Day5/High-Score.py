3683fa387111dfeefa8a73aba3bcb219e0ee883e
Yousef Mahmouud <57537303+yousefdotpy@users.noreply.github.com>
2022-07-08 02:31:47 +0200

    Create High-Score.py

---
+++ Day5/High-Score.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+for n in student_heights:
+    sum += n
+avg = round((sum / len(student_heights)),0)
+print(f"{int(avg)}")
+
+
+
