0456981d37a70bf5fb859749937dd762f8c22d26
Yousef Mahmouud <57537303+yousefdotpy@users.noreply.github.com>
2022-07-08 02:55:23 +0200

    Update Adding-even-numbers.py

--- Day5/Adding-even-numbers.py
+++ Day5/Adding-even-numbers.py
@@ -4,3 +4,11 @@ for num in range(2,101):
     if num % 2 == 0:
         total += num
 print(total)
+
+#total = 0
+#for num in range(2,101,2):
+#        total += num
+#print(total)
+
+#total = 2 * (50) * (51) /2
+#print(total)
